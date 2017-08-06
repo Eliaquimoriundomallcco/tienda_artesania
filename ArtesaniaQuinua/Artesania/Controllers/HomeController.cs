d696d6248f87f4f30dd4de6e1f4a18808e87ece6
Jose Antonio Maldonado Candela <josephcand@gmail.com>
2017-08-06 15:09:45 -0500

    arreglo

--- ArtesaniaQuinua/Artesania/Controllers/HomeController.cs
+++ ArtesaniaQuinua/Artesania/Controllers/HomeController.cs
@@ -32,17 +32,13 @@ namespace Artesania.Controllers
             return View();
         }
 
-<<<<<<< HEAD
-=======
-
->>>>>>> 0b49af7f45b6ce546ae05800f8d2d708f2953b92
 
         public ActionResult Email()
         {
             return View();
         }
 
-<<<<<<< HEAD
+
         public ActionResult Login(string usuario, string clave)
         {
             var u = bd.Cliente.FirstOrDefault(x => x.Usuario == usuario && x.Clave == clave);
@@ -75,8 +71,6 @@ namespace Artesania.Controllers
             }
         }
 
-
-=======
         public ActionResult Faq()
         {
             return View();
@@ -100,7 +94,4 @@ namespace Artesania.Controllers
         }
 
 
-
->>>>>>> 0b49af7f45b6ce546ae05800f8d2d708f2953b92
-    }
 }
